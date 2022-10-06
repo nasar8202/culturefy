1f34674814e2f545b62d0822d9a85ccf96d7416d
Hamza <hamza@binaryit.co>
2022-10-06 18:11:12 +0500

    answer crud successfully created

--- app/Models/BrandCultureAnswer.php
+++ app/Models/BrandCultureAnswer.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class BrandCultureAnswer extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'brand_culture_question_id',
+        'question'
+    ];
+    public function parent_question()
+    {
+        return $this->hasOne(BrandCultureQuestion::class, 'id', 'brand_culture_question_id');
+    }
 }
---
+++ resources/views/backend/superadmin/answers/editQuestion.blade.php
@@ -0,0 +1,107 @@
+@extends('backend.superadmin.layouts.app')
+@section('title','Edit Question Form')
+@section('secton')
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+
+<div class="page-heading">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Edit Question Form </h3>
+
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('superadmin') }}">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Edit Question Form </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- // Basic multiple Column Form section start -->
+    <section id="multiple-column-form">
+        <div class="row match-height">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Edit Question</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form class="form" action="{{ route('updateQuestion',$BrandCultureQuestion->id) }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+
+                                <div class="row">
+                                    <div class="col-md-12 mb-4 col-12">
+                                        <div class="col-md-12 mb-4 col-12">
+                                            <h6>Select Parent Category</h6>
+
+                                            <fieldset class="form-group">
+                                                <select class="form-select" name="id" id="basicSelect">
+                                                    <option value="" aria-readonly="">Select Main Category</option>
+                                                    @if ((count($brandParentCategory)) == 0)
+                                                    <option value="" aria-readonly="" disabled>No Category Found!</option>
+                                                    @else
+                                                    @foreach ($brandParentCategory as $category )
+                                                    
+                                                    <optgroup value="{{ $category->id }}" label="{{ $category->category_name }}">{{ $category->category_name }}</optgroup>
+                                                  
+                                                    @foreach($brandChildCategories as $child)
+                                                    @if($category->id == $child->parent_id)
+                                                     <option value="{{ $child->id }}"@if($BrandCultureQuestion->brand_culture_category_id == $child->id) selected @endif >{{ $child->category_name ?? 'No Category'  }}</option>
+                                                  
+                                                     @endif
+                                                   @endforeach
+                                                    @endforeach
+                                                    @endif
+    
+    
+                                                </select>
+                                                </select>
+                                            </fieldset>
+                                        </div>
+
+                                        <div class="col-md-12 mb-4 col-12">
+                                            <div class="form-group with-title mb-3">
+                                                <textarea class="form-control" id="exampleFormControlTextarea1"
+                                                    rows="3" name="question">{{ $BrandCultureQuestion->question }}</textarea>
+                                                <label>Type Your Question</label>
+                                            </div>
+                                        </div>
+
+                                            @if ($errors->has('question'))
+                                            <span class="text-danger">{{ $errors->first('question') }}</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="col-12 d-flex justify-content-end">
+                                        <button type="submit"
+                                            class="btn btn-primary me-1 mb-1">Submit</button>
+                                        <button type="reset"
+                                            class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- // Basic multiple Column Form section end -->
+</div>
+@endsection
